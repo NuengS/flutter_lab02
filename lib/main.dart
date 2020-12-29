4af3cdd882405ed0eb97d4713da026afe2e72323
DESKTOP-7F17K87\123 <614234020@parichat.skru.ac.th>
2020-12-29 16:39:01 +0700

    จบโปรแกรมแสดงหน้าฮีโร่ทั้งหมด +สกอ

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SupperHeros());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SupperHeros extends StatelessWidget {
+  final List<String> _heroImages = [
+    "images/bank.png",
+    "images/saitama.png",
+    "images/tasumagi.png",
+    "images/Genos.jpg",
+  ];
+
+  final List<String> _heroNames = [
+    "ซิลเวอร์ แฟง",
+    "ไซตามะ",
+    "ทัตสึมากิ",
+    "เจนอส",
+  ];
+
+  final List<String> _heroDetails = [
+    "ชื่อเดิมคือ แบงค์ Bang (バング) ชายชราผู้เป็นปรมาจารย์แห่งสำนักหมัดสายน้ำทลายหิน โดยพี่ชายของเขาชื่อ บอมบ์ Bomb (ボンブ) ก็ได้เปิดสำนักสอนการต่อสู้เช่นเดียว (หมัดลมหมุนตัดเหล็ก) ถึงแม้ตอนนี้เขาและพี่ชายจะมีอายุอานามมากแล้วก็ตาม แต่ร่างกายและพละกำลังของเขาก็ยังคงเทียบเคียงเมื่อสมัยก่อน เขายังถูกให้ระดับความแข็งแกร่งที่ระดับ 3 จากสมาคมฮีโร่อีกด้วย.",
+    "ไซตามะเป็นฮีโร่ที่แข็งแกร่งที่สุดในโลก ในทุกๆครั้ง เขาจะสามารถสยบคู่ต่อสู้ได้ภายในหมัดเดียว เขาเป็นผู้ชายอายุ 25 มีหัวโล้น เนื่องจากการฝึกแสนหฤโหดเพื่อที่จะได้มาเป็นฮีโร่ ไซตามะเป็นคนที่รักความยุติธรรม เขาได้ช่วยคนบริสุทธิ์จากเหล่าสิ่งมีชีวิตประหลาด และช่วยโลกมาแล้วนับครั้งไม่ถ้วน แต่เขาไม่เคยได้รับผลตอบแทนสักนิดเดียว.",
+    "ฮีโร่หญิงที่มีผมสีเขียวเรืองแสง ระดับ S แรงค์ 2 ผู้ได้รับสมญานามว่า ” Tornado of Terror” หรือที่แปลว่า “พายุทอร์นาโดแห่งความหวาดกลัว” เขามีความสามารถด้านพลังจิตที่แข็งแกร่ง จนสามารถยกสิ่งของหรือก้อนหินหนักๆ ได้ อีกทั้งยังเป็นเป็นพี่สาวของ Fubuki อีกด้วย.",
+    "ศิษย์เอกของไซตามะ เป็นไซบอร์กหรือหุ่นยนตร์ที่เกิดจากการดัดแปลงร่างกายโดย ดร.คุเซโนะ ซึ่งช่วยชีวิตเด็กหนุ่มในวัยเด็กหลังจากเขาต้องสูญเสียครอบครัวจากการเข้าจู่โจมของเหล่าไซบอร์กผู้ชั่วร้ายซึ่ง “บ้านแห่งวิวัฒนาการ” อยู่เบื้องหลัง ในด้านนิสัยนั้น.. เจนอสเป็นคนจริงจังกับทุกสิ่งและไม่ใส่ใจต่อชื่อเสียงผิดกับไซตามะ อย่างไรก็ตาม เจนอสเทิดทูนบูชาไซตามะอย่างมาก และหวังว่าสักวันอาจารย์ของเขาจะถ่ายทอดเคล็ดลับวิชาสู่ความแข็งแกร่งขั้นสุดยอดให้บ้าง.",
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+        home: Scaffold(
+      backgroundColor: Colors.yellow[700],
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.grey[850],
+        title: Text("SupperHeros"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(10),
+          children: [
+            Column(
+              children: [
+                // Container(
+                //   padding: EdgeInsets.all(10),
+                //   margin: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 20),
+                //   width: double.infinity,
+                //   color: Colors.grey[850],
+                //   child: Text(
+                //     "AAAA",
+                //     style: TextStyle(fontSize: 30),
+                //   ),
+                // ),
+                // Container(
+                //   padding: EdgeInsets.all(10),
+                //   margin: EdgeInsets.only(top: 0, left: 20, right: 20, bottom: 20),
+                //   width: double.infinity,
+                //   color: Colors.grey[850],
+                //   child: Text("BBBB"),
+                // ),
+                // Container(
+                //   padding: EdgeInsets.all(10),
+                //   margin: EdgeInsets.only(top: 0, left: 20, right: 20, bottom: 20),
+                //   width: double.infinity,
+                //   color: Colors.grey[850],
+                //   child: Text("CCCC"),
+                // ),
+                Hero(this._heroNames[0], this._heroImages[0],
+                    this._heroDetails[0]),
+                Hero(this._heroNames[1], this._heroImages[1],
+                    this._heroDetails[1]),
+                Hero(this._heroNames[2], this._heroImages[2],
+                    this._heroDetails[2]),
+                Hero(this._heroNames[3], this._heroImages[3],
+                    this._heroDetails[3]),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ));
+  }
+}
+
+class Hero extends StatelessWidget {
+  String _heroName;
+  String _heroImages;
+  String _heroDetail;
+
+  Hero(this._heroName, this._heroImages, this._heroDetail, {Key key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+          child: Column(
+        children: [
+          SizedBox(height: 10),
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage(this._heroImages),
+          ),
+          SizedBox(height: 10),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.flash_on),
+                Text(
+                  this._heroName,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+          Container(
+            child: Text(this._heroDetail),
+            padding: EdgeInsets.all(25),
+          ),
+        ],
+      )),
     );
   }
 }
